bc58657469f287861d92bd9656ead2f15961c12a
Debdhwoni <dhwoni77@gmail.com>
2024-10-05 15:51:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let me = {
+    myname: 'dhwoni',
+    age: 13,
+    school: 'jkphs',
+    classs: 7,
+    roll: 4
+}
+
+const {myname: name} = me
+const {age} = me
+const {school} = me
+const {classs} = me
+const {roll} = me
+
+console.log(name);
+
+console.log(roll);
+
